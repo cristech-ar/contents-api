63b23a8c167aaf65a708bd1d48b9ec2ceade7121
Cristian Rios <crios@toolboxtve.com>
2025-01-30 18:08:12 -0300

    deleting console.log

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -3,9 +3,7 @@ const { getMovies, addMovie } = require('../controllers/movieController');
 const { authenticate } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
-function test(){
-    console.log('llega')
-}
+
 router.get('/', authenticate, test);
 router.post('/', authenticate, addMovie);
 

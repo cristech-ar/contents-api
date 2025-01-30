88c18f1f9d0b0cbb3dd2675de4a7c06a7e16577d
Cristian Rios <crios@toolboxtve.com>
2025-01-30 18:28:28 -0300

    change port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const startServer = async () => {
 
     await connectToDatabase();
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 7000;
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}🚀`);
     });
